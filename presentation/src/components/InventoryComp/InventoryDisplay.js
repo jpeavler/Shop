cfc1f0a588c1221f0a76ca0940089b3c1a9b5885
Joshua Peavler <joshuaapeavler@gmail.com>
2020-05-27 16:00:56 -0600

    Delete button added to each item

--- presentation/src/components/InventoryComp/InventoryDisplay.js
+++ presentation/src/components/InventoryComp/InventoryDisplay.js
@@ -10,23 +10,26 @@ const InventoryDisplay = () => {
     useEffect(() => {
         getInv();
     }, []);
-
     const getInv = () => {
         fetch(`${process.env.REACT_APP_API_URL}/api/inventory`)
-            .then(response => response.json())
-            .then(inv => setInv(inv))
-            .then(setUpdate(false))
-            .then(setItemToUpdate(''))
+            .then(response => response.json()).then(inv => setInv(inv))
+            .then(setUpdate(false)).then(setItemToUpdate(''))
     }
     const handleUpdate = (item) => {
         setItemToUpdate(item);
         setUpdate(true);
+    }
+    const handleDelete = (id) => {
+        fetch(`${process.env.REACT_APP_API_URL}/api/inventory/${id}`, {
+            method: 'DELETE'
+        }).then(response => response.json()).then(getInv)
     }
     const displayInv = inventory.map((item) => {
         return (
             <div key={item._id} className="item">
                 <h4 className="itemname">{item.name}</h4>
                 <button onClick={() => handleUpdate(item)}>Edit</button>
+                <button onClick={() => handleDelete(item._id)}>Delete</button>
                 <p className="price">Price: ${item.price}</p>
                 <p className="quantity">Count: {item.quantity}</p>
                 <p className="description">{item.desc}</p>
@@ -40,7 +43,6 @@ const InventoryDisplay = () => {
     } else {
         renderForm = <InventoryForm key="additem" refresh={getInv} isUpdate={isUpdate}/>
     }
-
     return (
         <div key="inventory" className="inventory">
             <h1>Shop</h1>
@@ -49,5 +51,4 @@ const InventoryDisplay = () => {
         </div>
     )
 }
-
 export default InventoryDisplay;
