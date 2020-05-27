54cb465c87c8fabe01d0ab70ed311e9e2179b0e4
Joshua Peavler <joshuaapeavler@gmail.com>
2020-05-27 13:16:00 -0600

    Created skeleton for form

---
+++ presentation/src/components/InventoryForm.js
@@ -0,0 +1,19 @@
+import React, {useState} from 'react';
+
+const InventoryForm = ({refresh, myItem, id}) => {
+    return (
+        <form>
+            <input placeholder="" value={} type="" required/>
+            <input placeholder="" value={} type="" required/>
+            <input placeholder="" value={} type="" required/>
+            <input placeholder="" value={} type="" required/>
+            <input placeholder="" value={} type="" required/>
+            <input placeholder="" value={} type="" required/>
+            <input placeholder="" value={} type="" required/>
+            <input placeholder="" value={} type="" required/>
+            <input type="submit" value="Add Item"/>
+        </form>
+    )
+}
+
+export default InventoryForm;
