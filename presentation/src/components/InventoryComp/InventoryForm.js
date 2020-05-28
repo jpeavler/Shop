0bd753067ed489a74e9955390cc5773dba5a7c8b
Joshua Peavler <joshuaapeavler@gmail.com>
2020-05-28 13:55:02 -0600

    Changed add and edit item form into a modal

--- presentation/src/components/InventoryComp/InventoryForm.js
+++ presentation/src/components/InventoryComp/InventoryForm.js
@@ -1,20 +1,24 @@
 import React, {useState} from 'react';
+import {Modal} from 'reactstrap';
 
 const InventoryForm = ({refresh, myItem, id}) => {
     let formName = "";
     let formDesc = "";
     let formCount = "";
     let formPrice = "";
+    let modalOpen = false;
     if(myItem) {
         formName = myItem.name;
         formDesc = myItem.desc;
         formCount = myItem.quantity;
         formPrice = myItem.price;
+        modalOpen = true;
     }
     const [name, setName] = useState(formName);
     const [desc, setDesc] = useState(formDesc);
     const [quantity, setCount] = useState(formCount);
     const [price, setPrice] = useState(formPrice);
+    const [modal, setModal] = useState(modalOpen);
 
     const handleSubmit = (event) => {
         event.preventDefault();
@@ -28,7 +32,7 @@ const InventoryForm = ({refresh, myItem, id}) => {
                 body: JSON.stringify(updatedItem)
             }).then(() => setName(''))
                 .then(() => setDesc('')).then(() => setCount(''))
-                .then(() => setPrice('')).then(() => refresh())
+                .then(() => setPrice('')).then(() => refresh()).then(() => setModal(false))
         } else {
             const addedItem = {name, desc, quantity, price, isActive};
             fetch(`${process.env.REACT_APP_API_URL}/api/inventory`, {
@@ -37,7 +41,7 @@ const InventoryForm = ({refresh, myItem, id}) => {
                 body: JSON.stringify(addedItem)
             }).then(() => setName(''))
             .then(() => setDesc('')).then(() => setCount(''))
-            .then(() => setPrice('')).then(() => refresh())
+            .then(() => setPrice('')).then(() => refresh()).then(() => setModal(false))
         }
     }
 
@@ -50,20 +54,27 @@ const InventoryForm = ({refresh, myItem, id}) => {
         renderSubmit = <input key="add" value="Add Item" type="submit"/>
     }
 
+    const toggle = () => setModal(!modal);
+
     return (
-        <form onSubmit={handleSubmit}>
-            <input placeholder="Item Name" value={name} 
-                type="text" onChange={({target}) => setName(target.value)} required/>
-            <textarea placeholder="Description" value={desc} 
-                onChange={({target}) => setDesc(target.value)}/>
-            <input placeholder="Item Count" value={quantity} type="number" 
-                onChange={({target}) => setCount(target.value)} required/>
-            <input placeholder="Price" value={price}
-                type="number" min=".01" step=".01" 
-                onChange={({target}) => setPrice(target.value)} required/>
-            {renderSubmit}
-            {cancel}
-        </form>
+        <>
+            <button onClick={toggle}>Add New Item</button>
+            <Modal isOpen={modal} toggle={toggle} className="modelform">
+                <form onSubmit={handleSubmit}>
+                    <input placeholder="Item Name" value={name} 
+                        type="text" onChange={({target}) => setName(target.value)} required/>
+                    <textarea placeholder="Description" value={desc} 
+                        onChange={({target}) => setDesc(target.value)}/>
+                    <input placeholder="Item Count" value={quantity} type="number" 
+                        onChange={({target}) => setCount(target.value)} required/>
+                    <input placeholder="Price" value={price}
+                        type="number" min=".01" step=".01" 
+                        onChange={({target}) => setPrice(target.value)} required/>
+                    {renderSubmit}
+                    {cancel}
+                </form>
+            </Modal>
+        </>
     )
 }
 
