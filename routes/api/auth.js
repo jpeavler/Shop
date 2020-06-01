f2d1f138d8595b9ef229226fb034f784914ec91b
Joshua Peavler <joshuaapeavler@gmail.com>
2020-06-01 09:57:26 -0600

    Implemented RESTful encription during register user router

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,14 +1,29 @@
 const express = require('express');
 const router = express.Router();
-const {
-    registerUser
-} = require('../../dal/auth')
+const bcrypt = require('bcrypt');
+const {registerUser} = require('../../dal/auth');
+
+const salt = process.env.SALT;
 
 //Post router
-router.post('/register', async function(req, res) {
+router.post('/register', function(req, res) {
     try {
-        const newItem = await registerUser(req.body);
-        res.send(newItem);
+        const saltRounds = +salt;
+        let body = req.body;
+        const myPlaintextPassword = req.body.password;
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            if(err) {
+                throw err;
+            }
+            bcrypt.hash(myPlaintextPassword, salt, async function(err, hash) {
+                if(err) {
+                    throw err;
+                }
+                body.password = hash;
+                const user = await registerUser(req.body);
+                res.send(user);
+            });
+        });
     } catch(err) {
         if(err.error) {
             res.status(400).send(err);
@@ -18,3 +33,5 @@ router.post('/register', async function(req, res) {
         }
     }
 });
+
+module.exports = router;
