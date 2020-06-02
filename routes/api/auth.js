9d60b6139a3e79e296bd91602bb84fe1d5793f01
Joshua Peavler <joshuaapeavler@gmail.com>
2020-06-02 14:44:34 -0600

    Implemented token creation in RESTful login post in API

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,11 +1,46 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
-const {registerUser} = require('../../dal/auth');
+const jwt = require('jsonwebtoken');
+const {registerUser, getUserByValue} = require('../../dal/auth');
 
 const salt = process.env.SALT;
+const privateKey = process.env.PRIVATE_KEY;
 
-//Post router
+//Post Login router
+router.post('/login', async function(req, res) {
+    try {
+        const dbUser = await getUserByValue('username', req.body.username);
+        if(dbUser.length === 0) {
+            res.status(401).send('Login Failed');
+            console.log(`${req.body.username} doesn't exist`);
+        } else if(dbUser.length > 1) {
+            res.status(500).send('Login Failed')
+            console.log(`${req.body.username} exists more than once`)
+        } else {
+            bcrypt.compare(req.body.password, dbUser[0].password, function(err, result) {
+                if(err) {throw err;}
+                if(!result) {
+                    res.status(401).send('Login Failed');
+                    console.log(`Provided password for ${req.body.username} doesn't match`);
+                } else {
+                    let expire = new Date(Date.now());      //makes the web token expire in a week
+                    expire.setDate(expire.getDate() + 7);
+                    jwt.sign({expires: expire, _id: dbUser[0]._id }, privateKey, { algorithm: 'HS512' }, function(err, token) {
+                        if(err) {throw err;}
+                        console.log(token);
+                        res.set('authentication', token);
+                        res.send();
+                    });
+                }
+            });
+        }
+    } catch(err) {
+        console.log(err);
+        res.status(500).send('Internal Server issue, check logs');
+    }
+})
+//Post Register router
 router.post('/register', function(req, res) {
     try {
         const saltRounds = +salt;
