ea6191e8abeb5dd4704ff4edd570260182e5d45c
Joshua Peavler <joshuaapeavler@gmail.com>
2020-06-03 15:46:55 -0600

    Implemented a loading spinner for Registration

--- presentation/src/components/UserComp/SignupForm.js
+++ presentation/src/components/UserComp/SignupForm.js
@@ -1,14 +1,15 @@
-import React, {useState, useEffect} from 'react';
-import {Form, Input, Button} from 'reactstrap'
+import React, {useState} from 'react';
+import {Form, Input, Button, Spinner} from 'reactstrap'
 
 const SignupForm = () => {
     const [username, setUserName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [pswrdconfirm, setConfirm] = useState('');
-
+    const [displaySpinner, setSpinner] = useState(false);
     const handleSubmit = (event) => {
         event.preventDefault();
+        setSpinner(true);
         if(password === pswrdconfirm){
             const isActive = true;
             const bio = "Shop user";
@@ -24,15 +25,22 @@ const SignupForm = () => {
         else {
             //message to warn user that passwords don't match
         }
+        setSpinner(false);
     }
+    let spinner;
+    if(displaySpinner) {spinner = <Spinner color="primary"/>}
+    else {spinner = <></>}
     return (
-        <Form className="signup" onSubmit={handleSubmit}>
-            <Input placeholder="User Name" onChange={({target}) => setUserName(target.value)} value={username} required/>
-            <Input placeholder="Email" type="email" onChange={({target}) => setEmail(target.value)} value={email} required/>
-            <Input placeholder="Password" type="password" onChange={({target}) => setPassword(target.value)} value={password} required/>
-            <Input placeholder="Password Confirmation" type="password" onChange={({target}) => setConfirm(target.value)} value={pswrdconfirm} required/>
-            <Button>Submit</Button>
-        </Form>
+        <>
+            <Form className="signup" onSubmit={handleSubmit}>
+                <Input placeholder="User Name" onChange={({target}) => setUserName(target.value)} value={username} required/>
+                <Input placeholder="Email" type="email" onChange={({target}) => setEmail(target.value)} value={email} required/>
+                <Input placeholder="Password" type="password" onChange={({target}) => setPassword(target.value)} value={password} required/>
+                <Input placeholder="Password Confirmation" type="password" onChange={({target}) => setConfirm(target.value)} value={pswrdconfirm} required/>
+                <Button block>Submit</Button>   
+            </Form>
+            {spinner}
+        </>
     )
 }
 
