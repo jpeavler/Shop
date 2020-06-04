e22b8f2286359eb0afd31d9ecdbf58134ad0fa39
Joshua Peavler <joshuaapeavler@gmail.com>
2020-06-04 16:49:23 -0600

    Setup Profile page to display bio and default profile picture

--- presentation/src/pages/Profile.js
+++ presentation/src/pages/Profile.js
@@ -1,13 +1,36 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import {Button, Media} from 'reactstrap';
 import NavBar from '../components/NavBar'
 import {isLoggedIn} from '../config/auth';
 
 const Profile = () => {
-    const [loggedIn, setLoggedIn] = useState(isLoggedIn());
+    const [loggedIn, setLoggedIn] = useState(isLoggedIn());     //loggedIn returns the token if the user is logged in
+    const [user, setUser] = useState("");
+
+    const getUserInfo = () => {
+        fetch(`${process.env.REACT_APP_API_URL}/api/auth/id/${loggedIn}`)
+        .then(response => response.json()).then(userInfo => setUser(userInfo))
+    }
+    useEffect(() => {
+        getUserInfo();
+    }, []);
+
+        console.log(user.pic);
+        let img;
+        if(user.pic == 0) {img =<Media object src={"/assets/DefaultUserIcon.png"} alt="Profile" width="100px"/>}
     return (
         <div className="profile">
             <NavBar loggedIn={loggedIn} setLoggedIn ={setLoggedIn}/>
-            <h2>Welcome to your Profile</h2>
+            <Media>
+                <Media left>
+                    {img}
+                </Media>
+                <Media body>
+                    <Media heading>Welcome {user.username} to your Profile</Media>
+                    {user.bio}
+                    <Button>Update Bio</Button>
+                </Media>
+            </Media>
         </div>
     )
 }
