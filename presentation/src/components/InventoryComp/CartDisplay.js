695bf726761c94b2c37f4a980f5dbdf731d69bce
Joshua Peavler <joshuaapeavler@gmail.com>
2020-06-10 17:01:34 -0600

    Display user cart in table format

--- presentation/src/components/InventoryComp/CartDisplay.js
+++ presentation/src/components/InventoryComp/CartDisplay.js
@@ -1,36 +1,42 @@
 import React, {useState, useEffect} from 'react';
+import {Table, Card, CardHeader, CardBody} from 'reactstrap';
 
 const CartDisplay = () => {
-    const [inventory, setInv] = useState([]);
+    const [cartItems, setCartItems] = useState([]);
     let cart = localStorage.getItem('Cart');
     useEffect(() => {
         getInv();
     }, []);
     const getInv = () => {
-        fetch(`${process.env.REACT_APP_API_URL}/api/inventory`)
-            .then(response => response.json()).then(inv => {
+        fetch(`${process.env.REACT_APP_API_URL}/api/inventory`).then(response => response.json())
+            .then(inv => {
                 let invInCart = Object.assign([], inv);
                 invInCart = invInCart.filter((item) => {
                     let keep = false;
                     if(cart != null) {
                         let cartParsed = JSON.parse(cart);
                         cartParsed.forEach((itemID) => {
-                            console.log("itemID: ", itemID);
-                            console.log("item._id: ", item._id);
-                            if(itemID == item._id) {
-                                console.log("It happened")
-                                keep = true;
-                            }
+                            if(itemID == item._id) {keep = true;}
                         })
                     }
                     return keep;
                 })
-                setInv(invInCart);
+                setCartItems(invInCart);
             })
     }
-    console.log("Inventory: ", inventory);
+    const displayInv = cartItems.map((item) => {
+        return (
+            <tr key={item._id} className="item">
+                <td className="itemname">{item.name}</td>
+                <td>${item.price}</td>
+            </tr>
+        )
+    });
     return (
-        <p>Cart: {cart}</p>
+        <Table striped>
+        <thead><th>Product Name</th><th>Price</th></thead>
+        <tbody>{displayInv}</tbody>
+        </Table>
     )
 }
 
